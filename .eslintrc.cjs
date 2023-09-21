681193b611ca96bb4ac3cb260b7ad77c289c61fc
wtchoi <wtchoi@mobidays.com>
2023-09-21 14:09:56 +0900

    init

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution');
+
+module.exports = {
+    root: true,
+    extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/eslint-config-prettier/skip-formatting'],
+    env: {
+        'vue/setup-compiler-macros': true
+    },
+    parserOptions: {
+        ecmaVersion: 'latest'
+    },
+    rules: {
+        'prettier/prettier': [
+            'error',
+            {
+                singleQuote: true,
+                semi: true,
+                tabWidth: 4,
+                trailingComma: 'none',
+                printWidth: 140,
+                bracketSpacing: true,
+                arrowParens: 'avoid'
+            }
+        ]
+    }
+};
